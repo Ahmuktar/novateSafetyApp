8a18fa25efaffb07fbf07bc2e9cbf9cc5a537fbe
Ahmuktar <ahmuktar203@gmail.com>
2024-09-29 08:44:40 +0100

    "Updated CSS variables, button component, and welcome page layout; refactored button variants and tailwind config."

--- app/page.js
+++ app/page.js
@@ -23,38 +23,37 @@ export default function WelcomePage() {
 
         {/* Welcome Content */}
         <main className="flex-grow flex flex-col h-full justify-center items-center px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col space-y-6 items-center w-1/3">
-            <div className="mb-20">
+          <div className="flex flex-col items-center">
+            <div className="mb-6">
               <Image
-                src="/images/icon.png"
+                src="/images/icon3.png"
                 width={200}
                 height={200}
                 alt="Logo"
-                className="rounded-full dark:hidden"
+                className="block dark:hidden"
               />
               <Image
                 src="/images/icon2.png"
                 width={200}
                 height={200}
                 alt="Logo"
-                className="rounded-full light:hidden"
+                className="hidden dark:block"
               />
             </div>
             <div className="">
-              <h1 className="text-4xl font-bold text-center mb-6">
-                Welcome to the Novate Safety App
+              <h1 className="text-5xl font-bold text-center mb-4">
+                Novate Safety App
               </h1>
-              <p className="text-xl text-center mb-8 max-w-2xl">
-                Your trusted platform for reporting Adverse Drug Reactions
-                (ADRs).
+              <p className="text-xl font-semibold text-center max-w-2xl">
+                Report Adverse Drug Reactions (ADRs) securely and efficiently
               </p>
-              <p className="text-sm text-center mb-4 max-w-2xl">
+              <p className="text-sm text-center text-muted-foreground mt-10 mb-6 max-w-xl">
                 Before you start reporting, please read and accept our Terms and
                 Conditions. This helps ensure you understand our policies and
                 how we handle your reports.
               </p>
             </div>
-            <div className="w-full items-center flex flex-col max-w-md">
+            <div className="w-full items-center gap-10 flex flex-col max-w-md">
               <div className="flex items-center mb-4">
                 <input
                   type="checkbox"
@@ -63,7 +62,7 @@ export default function WelcomePage() {
                   checked={acceptedTerms}
                   onChange={(e) => setAcceptedTerms(e.target.checked)}
                 />
-                <label htmlFor="acceptTerms" className="text-sm">
+                <label htmlFor="acceptTerms" className="text-base">
                   I accept the{" "}
                   <Link href="/" className="underline">
                     Terms and Conditions
@@ -71,7 +70,7 @@ export default function WelcomePage() {
                 </label>
               </div>
               <form className="flex items-center">
-                <Button type="submit" disabled={!acceptedTerms}>
+                <Button type="submit" className="" disabled={!acceptedTerms}>
                   Start Reporting
                 </Button>
               </form>
